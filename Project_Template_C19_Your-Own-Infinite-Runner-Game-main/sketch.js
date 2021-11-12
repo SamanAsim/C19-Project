92e1e5e542127adb46cae01c814243913fd63d3a
Saman <87480225+SamanAsim@users.noreply.github.com>
2021-11-12 22:36:53 +1100

    Add files via upload

---
+++ Project_Template_C19_Your-Own-Infinite-Runner-Game-main/sketch.js
@@ -0,0 +1,18 @@
+
+
+
+function preload(){
+WomanRunning= loadAnimation("Running_1.PNG","Running_2.PNG","Running_3.PNG");
+HealthyFood= loadAnimation("apple.PNG","banana.PNG","watermelon.PNG");
+JunkFood= loadAnimation("burger.PNG","chips.PNG","soda.PNG");
+RunningTrack= loadImage("Running_Track.PNG");
+}
+
+function setup() {
+ createCanvas(200.600);
+ 
+}
+
+function draw() {
+ WomanRunning1= display;
+}
